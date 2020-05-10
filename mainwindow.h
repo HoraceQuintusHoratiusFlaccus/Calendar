01ae1c43edb7fec7c0e48b8c52680d2e0a371819
Cho-yeon <HoraceQuintusHoratiusFlaccus@gmail.com>
2020-05-10 13:03:42 +0900

    CREATE: mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,82 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QCalendarWidget;
+class QCheckBox;
+class QComboBox;
+class QDate;
+class QDateEdit;
+class QGridLayout;
+class QGroupBox;
+class QLabel;
+QT_END_NAMESPACE
+
+class Window : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Window(QWidget * Parent = nullptr);
+
+private slots:
+    void LocaleChanged(int);
+    void FirstDayChanged(int);
+    void SelectionModeChanged(int);
+    void HorizontalHeaderChanged(int);
+    void VerticalHeaderChanged(int);
+    void SelectedDateChanged();
+    void MinDateChanged(QDate);
+    void MaxDateChanged(QDate);
+    void WeekdayFormatChanged();
+    void WeekendFormatChanged();
+    void ReformatHeaders();
+    void ReformatCalendarPage();
+
+private:
+    void CreatePreviewGroupBox();
+    void CreateGeneralOptionsGroupBox();
+    void CreateDatesGroupBox();
+    void CreateTextFormatsGroupBox();
+    QComboBox * CreateColorComboBox();
+
+    QGroupBox * PreviewGroupBox;
+    QGridLayout * PreviewLayout;
+    QCalendarWidget * Calendar;
+
+    QGroupBox * GeneralOptionsGroupBox;
+    QLabel * LocaleLabel;
+    QLabel * FirstDayLabel;
+    QLabel * SelectionModeLabel;
+    QLabel * HorizontalHeaderLabel;
+    QLabel * VerticalHeaderLabel;
+    QComboBox * LocaleCombo;
+    QComboBox * FirstDayCombo;
+    QComboBox * SelectionModeCombo;
+    QCheckBox * GridCheckBox;
+    QCheckBox * navigationCheckBox;
+    QComboBox * HorizontalHeaderCombo;
+    QComboBox * VerticalHeaderCombo;
+
+    QGroupBox * DatesGroupBox;
+    QLabel * CurrentDateLabel;
+    QLabel * MinDateLabel;
+    QLabel * MaxDateLabel;
+    QDateEdit * CurrentDateEdit;
+    QDateEdit * MinDateEdit;
+    QDateEdit * MaxDateEdit;
+
+    QGroupBox * TextFormatsGroupBox;
+    QLabel * WeekdayColorLabel;
+    QLabel * WeekendColorLabel;
+    QLabel * HeaderTextFormatLabel;
+    QComboBox * WeekdayColorCombo;
+    QComboBox * HeaderTextFormatCombo;
+
+    QCheckBox * FirstFirdayCheckBox;
+    QCheckBox * MayFirstCheckBox;
+};
+
+#endif // MAINWINDOW_H
